0c4b1c8bcdf4e6457f731d18e0feaadc9f452083
Tom Cocca <tom.cocca@gmail.com>
2012-05-03 16:29:52 -0400

    updated some caching, no need for an attr_accessor when a simple instance method does the trick

--- lib/rentjuicer/listings.rb
+++ lib/rentjuicer/listings.rb
@@ -1,34 +1,33 @@
 module Rentjuicer
   class Listings
-    
+
     attr_accessor :client, :resource
-    
+
     def initialize(client)
       self.client = client
       self.resource = "/listings.json"
     end
-    
+
     def search(params = {})
       limit = params[:limit] || 20
       params[:order_by] ||= "rent"
       params[:order_direction] ||= "asc"
       SearchResponse.new(self.client.process_get(resource, params), limit)
     end
-    
+
     def featured(params = {})
       params.merge!(:featured => 1)
       search(params)
     end
-    
+
     def find_by_id(listing_id, params = {})
       response = SearchResponse.new(self.client.process_get(resource, params.merge(:rentjuice_id => listing_id)))
       (response.success? && response.properties.size > 0) ? response.properties.first : nil
     end
-    
+
     def find_all(params = {})
       per_page = params[:limit] || 20
       all_listings = []
-      
       response = search(params)
       if response.success?
         all_listings << response.properties
@@ -44,7 +43,7 @@ module Rentjuicer
       end
       all_listings.flatten
     end
-    
+
     def find_all_by_ids(listing_ids, params = {})
       listing_ids = listing_ids.split(',') if listing_ids.is_a?(String)
       all_listings = []
@@ -54,49 +53,49 @@ module Rentjuicer
       end
       all_listings.flatten
     end
-    
+
     class SearchResponse < Rentjuicer::Response
-      
-      attr_accessor :limit, :paginator_cache
-      
+
+      attr_accessor :limit
+
       def initialize(response, limit = 20)
         super(response)
         @limit = limit
       end
-      
+
       def properties
-        return [] if !self.success? || self.body.listings.blank?
         @cached_properties ||= begin
           props = []
-          self.body.listings.each do |listing|
-            props << Rentjuicer::Listing.new(listing)
+          if self.success? && !self.body.listings.blank?
+            self.body.listings.each do |listing|
+              props << Rentjuicer::Listing.new(listing)
+            end
           end
           props
         end
       end
-      
+
       def total_results
         self.body.total_count ? self.body.total_count : properties.size
       end
-      
+
       def mls_results?
         @has_mls_properties ||= properties.any?{|property| property.mls_listing?}
       end
-      
+
       def mls_disclaimers
         @disclaimers ||= properties.collect{|property| property.mls_disclaimer}.compact.uniq
       end
-      
+
       def paginator
-        paginator_cache if paginator_cache
-        self.paginator_cache = WillPaginate::Collection.create(
-          self.body.page ||= 1, 
-          @limit, 
+        @paginator_cache ||= WillPaginate::Collection.create(
+          self.body.page ||= 1,
+          @limit,
           self.total_results) do |pager|
           pager.replace properties
         end
       end
     end
-    
+
   end
 end
